a8b409d6b27723c93a3ba20aeb1d7a6156d12f1a
bariluz93 <bar.iluz@mail.huji.ac.il>
2022-07-01 19:10:08 +0300

    automated words to debias, and added professions accuracies print

--- debiaswe/debias.py
+++ debiaswe/debias.py
@@ -2,11 +2,11 @@ from __future__ import print_function, division
 import sys
 sys.path.append("..") # Adds higher directory to python modules path.
 from debiaswe.debiaswe import we
+from debias_files.consts import WordsToDebias
 import json
 import numpy as np
 import argparse
 import sys
-from scipy import spatial
 if sys.version_info[0] < 3:
     import io
     open = io.open
@@ -20,67 +20,65 @@ Tolga Bolukbasi, Kai-Wei Chang, James Zou, Venkatesh Saligrama, and Adam Kalai
 """
 
 
-def debias(E, gender_specific_words, definitional, equalize, professions):
+def debias(E, gender_specific_words, definitional, equalize, professions, words_to_debias=1):
     gender_direction = we.doPCA(definitional, E).components_[0]
-    # specific_set = set(gender_specific_words)
-    # for i, w in enumerate(E.words):
-    #     if w not in specific_set:
-    #         a=we.drop(E.vecs[i], gender_direction)
-    #         b=E.vecs[i]
-    #         E.vecs[i] = we.drop(E.vecs[i], gender_direction)
-    #
-    # E.normalize()
-    # candidates = {x for e1, e2 in equalize for x in [(e1.lower(), e2.lower()),
-    #                                                  (e1.title(), e2.title()),
-    #                                                  (e1.upper(), e2.upper())]}
-    # print(candidates)
-    # for (a, b) in candidates:
-    #     if (a in E.index and b in E.index):
-    #         y = we.drop((E.v(a) + E.v(b)) / 2, gender_direction)
-    #         z = np.sqrt(1 - np.linalg.norm(y)**2)
-    #         if (E.v(a) - E.v(b)).dot(gender_direction) < 0:
-    #             z = -z
-    #         E.vecs[E.index[a]] = z * gender_direction + y
-    #         E.vecs[E.index[b]] = -z * gender_direction + y
-    # E.normalize()
+    if words_to_debias==WordsToDebias.ALL_VOCAB.value:
+        specific_set = set(gender_specific_words)
+        for i, w in enumerate(E.words):
+            if w not in specific_set:
+                E.vecs[i] = we.drop(E.vecs[i], gender_direction)
 
-    for i, w in enumerate(E.words):
-        if w in professions:
-            E.vecs[i] = we.drop(E.vecs[i], gender_direction)
-    # E.normalize()
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("embedding_filename", help="The name of the embedding")
-    parser.add_argument("definitional_filename", help="JSON of definitional pairs")
-    parser.add_argument("gendered_words_filename", help="File containing words not to neutralize (one per line)")
-    parser.add_argument("equalize_filename", help="???.bin")
-    parser.add_argument("debiased_filename", help="???.bin")
-
-    args = parser.parse_args()
-    print(args)
-
-    with open(args.definitional_filename, "r") as f:
-        defs = json.load(f)
-    print("definitional", defs)
-
-    with open(args.equalize_filename, "r") as f:
-        equalize_pairs = json.load(f)
-
-    with open(args.gendered_words_filename, "r") as f:
-        gender_specific_words = json.load(f)
-    print("gender specific", len(gender_specific_words), gender_specific_words[:10])
-
-    E = we.WordEmbedding(args.embedding_filename)
-
-    print("Debiasing...")
-    debias(E, gender_specific_words, defs, equalize_pairs)
-
-    print("Saving to file...")
-    if args.embedding_filename[-4:] == args.debiased_filename[-4:] == ".bin":
-        E.save_w2v(args.debiased_filename)
+        E.normalize()
+        candidates = {x for e1, e2 in equalize for x in [(e1.lower(), e2.lower()),
+                                                         (e1.title(), e2.title()),
+                                                         (e1.upper(), e2.upper())]}
+        print(candidates)
+        for (a, b) in candidates:
+            if (a in E.index and b in E.index):
+                y = we.drop((E.v(a) + E.v(b)) / 2, gender_direction)
+                z = np.sqrt(1 - np.linalg.norm(y)**2)
+                if (E.v(a) - E.v(b)).dot(gender_direction) < 0:
+                    z = -z
+                E.vecs[E.index[a]] = z * gender_direction + y
+                E.vecs[E.index[b]] = -z * gender_direction + y
+        E.normalize()
     else:
-        E.save(args.debiased_filename)
+        for i, w in enumerate(E.words):
+            if w in professions:
+                E.vecs[i] = we.drop(E.vecs[i], gender_direction)
 
-    print("\n\nDone!\n")
+# if __name__ == "__main__":
+#
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("embedding_filename", help="The name of the embedding")
+#     parser.add_argument("definitional_filename", help="JSON of definitional pairs")
+#     parser.add_argument("gendered_words_filename", help="File containing words not to neutralize (one per line)")
+#     parser.add_argument("equalize_filename", help="???.bin")
+#     parser.add_argument("debiased_filename", help="???.bin")
+#
+#     args = parser.parse_args()
+#     print(args)
+#
+#     with open(args.definitional_filename, "r") as f:
+#         defs = json.load(f)
+#     print("definitional", defs)
+#
+#     with open(args.equalize_filename, "r") as f:
+#         equalize_pairs = json.load(f)
+#
+#     with open(args.gendered_words_filename, "r") as f:
+#         gender_specific_words = json.load(f)
+#     print("gender specific", len(gender_specific_words), gender_specific_words[:10])
+#
+#     E = we.WordEmbedding(args.embedding_filename)
+#
+#     print("Debiasing...")
+#     debias(E, gender_specific_words, defs, equalize_pairs)
+#
+#     print("Saving to file...")
+#     if args.embedding_filename[-4:] == args.debiased_filename[-4:] == ".bin":
+#         E.save_w2v(args.debiased_filename)
+#     else:
+#         E.save(args.debiased_filename)
+#
+#     print("\n\nDone!\n")
